838c47564716c56ceaca42fb1e61b9565d45209b
Alex Kalabukhov <a.v.kalabuhov@gmail.com>
2022-04-03 18:45:55 +0300

    Initial project

---
+++ Components/Clipper/Delphi/main demo/clipper_demo.dpr
@@ -0,0 +1,18 @@
+// JCL_DEBUG_EXPERT_GENERATEJDBG OFF
+// JCL_DEBUG_EXPERT_INSERTJDBG OFF
+// JCL_DEBUG_EXPERT_DELETEMAPFILE OFF
+program clipper_demo;
+
+uses
+  Forms,
+  main in 'main.pas' {MainForm},
+  GR32_Misc in 'GR32_Misc.pas',
+  clipper in '..\clipper.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TMainForm, MainForm);
+  Application.Run;
+end.
